95ac62686ef58a8a26b28daef0d9cc3ab5b0b78b
Erik Montnemery <erik@montnemery.com>
2026-09-14 21:54:20 +0200

    Drop use of aioresponses (#146)

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ from collections.abc import Generator
 import logging
 from unittest.mock import AsyncMock, patch
 
-from aioresponses import aioresponses
 from homeassistant.const import CONF_NAME, CONF_PASSWORD, CONF_URL, CONF_USERNAME
 import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
@@ -26,13 +25,6 @@ def mock_setup_entry() -> Generator[AsyncMock]:
         return_value=True,
     ) as mock_setup_entry:
         yield mock_setup_entry
-
-
-@pytest.fixture(name="responses")
-def aioresponses_fixture() -> Generator[aioresponses]:
-    """Return aioresponses fixture."""
-    with aioresponses() as mocked_responses:
-        yield mocked_responses
 
 
 @pytest.fixture
